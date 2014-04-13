97688c0b3ca12558955585ef3849212aaeaf7a7c
fungku <fungku@gmail.com>
2014-04-12 17:05:21 -0700

    initial commit

---
+++ src/Fungku/HubSpot/Facades/HubSpot.php
@@ -0,0 +1,16 @@
+<?php namespace Fungku\HubSpot\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class HubSpot extends Facade {
+	
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor()
+	{
+		return 'hubspot';
+	}
+}
---
+++ src/config/api.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+		
+	'key' => 'hubspot_api_key',
+
+	'useragent' => 'Default_user_agent',
+
+);
