57768388ad2bab6574b55d34d243efd8984f6bbd
Ryan Winchester <fungku@users.noreply.github.com>
2014-07-02 20:17:44 -0700

    Update api.php

--- src/config/api.php
+++ src/config/api.php
@@ -8,7 +8,7 @@ return array(
 	|--------------------------------------------------------------------------
 	|
 	| This is the API Key for your hubspot account. By default we look for an
-	| environment variable. You can set these using dot files.
+	| environment variable. You can set these using dot files in Laravel.
 	| 
 	| e.g. in .env.php in production or .env.local.php in local environment
 	|
